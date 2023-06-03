6371f6aff76594798d77ffbd7cd468b65068524d
Gaagul Gigi <gaagulgigi@gmail.com>
2023-06-03 16:04:44 +0530

    Added property card

--- app/javascript/src/components/main.jsx
+++ app/javascript/src/components/main.jsx
@@ -1,20 +1,8 @@
 import React from 'react'
-import PropertyCard from './PropertyCard';
-import Login from './Login';
-import SignUp from './Signup';
 
 const Main = () => {
-  const HOUSE = {
-    name: "Modern home in city center",
-    location: "New York",
-    price: 1900000,
-    beds: 3,
-    baths: 2,
-    area: 200,
-  };
-
   return (
-    <PropertyCard house={HOUSE}/>
+    <div>Main</div>
   )
 }
 
