66caa8e8b3cee6e3b43b0ee083a5e7695f2415cf
Gaagul Gigi <gaagulgigi@gmail.com>
2023-06-03 14:29:06 +0530

    Added material ui and revamped Login and SignUp page

--- app/javascript/src/components/main.jsx
+++ app/javascript/src/components/main.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import PropertyCard from './PropertyCard';
+import Login from './Login';
+import SignUp from './Signup';
 
 const Main = () => {
+  const HOUSE = {
+    name: "Modern home in city center",
+    location: "New York",
+    price: 1900000,
+    beds: 3,
+    baths: 2,
+    area: 200,
+  };
+
   return (
-    <div>main</div>
+    <PropertyCard house={HOUSE}/>
   )
 }
 
