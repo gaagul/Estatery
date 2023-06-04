06ce7f1280a4aab15472521c63252c2f2acd8086
Gaagul Gigi <gaagulgigi@gmail.com>
2023-06-04 17:26:00 +0530

    Added tabs to admin panel

--- app/javascript/src/components/AdminPanel/Header.jsx
+++ app/javascript/src/components/AdminPanel/Header.jsx
@@ -1,20 +1,14 @@
 import React from "react";
-import { OutlinedInput, Button } from "@mui/material";
+import { OutlinedInput, Button, Tabs, Tab } from "@mui/material";
 
-const Header = () => (
-  <header className="flex h-16 items-center justify-between">
-    <h1 className="text-lg font-semibold">Approval requests</h1>
-    <div className="flex items-center">
-      <OutlinedInput
-        placeholder="Search"
-        size="small"
-        style={{ maxHeight: "30px" }}
-        type="text"
-      />
-      <Button className="rounded bg-white px-4 py-2 font-semibold text-purple-700">
-        Search
-      </Button>
-    </div>
+const Header = ({tab, setTab}) => (
+  <header className="flex h-16 items-center gap-10">
+    <h1 className="text-lg font-semibold">Requests: 100</h1>
+    <Tabs value={tab} onChange={setTab} aria-label="basic tabs example">
+      <Tab label="Pending" />
+      <Tab label="Approved" />
+      {/* <Tab label="Services" /> */}
+    </Tabs>
   </header>
 );
 
