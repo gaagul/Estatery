dab271a6165ee1b7f5493f4499df07427e79d614
navaneeth-krishna <naveekurosaki@gmail.com>
2023-06-04 17:38:03 +0530

    Added filterbar component and a testpage

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom/client";
 import Main from "./components/main";
 import "./stylesheets/main.scss";
 import Homepage from "./pages/Home";
+import Testpage from "./pages/test";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Homepage />
+    <Testpage />
   </React.StrictMode>
 );
