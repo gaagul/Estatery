16fe5c6bfb0bf84f1946834f55e836d40d31fcf6
Gaagul Gigi <70290286+gaagul@users.noreply.github.com>
2023-06-04 17:26:23 +0530

    Added tabs to admin panel (#15)

--- app/javascript/src/components/AdminPanel/Sidebar.jsx
+++ app/javascript/src/components/AdminPanel/Sidebar.jsx
@@ -5,7 +5,7 @@ import { ADMIN_OPTIONS } from "../constants";
 const SideBar = () => (
   <div className="flex h-screen w-16 flex-col items-center justify-between py-4 text-gray-500 secondary">
     <div className="mt-6 h-16 text-lg font-bold uppercase">LOGO</div>
-    <div className="flex flex-col items-center justify-start">
+    <div className="flex flex-col items-center justify-start gap-8">
       {ADMIN_OPTIONS.map((option, index) => (
         <button
           className="flex h-12 w-12 items-center justify-center rounded-lg transition-colors hover:bg-purple-100 focus:bg-purple-100"
