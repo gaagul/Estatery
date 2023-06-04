a0d29b0e494e1e53a0e4299fce636805c0c49258
Gaagul Gigi <70290286+gaagul@users.noreply.github.com>
2023-06-04 19:20:13 +0530

    Added localStorageFunctions and updated table Data (#17)

--- app/javascript/src/components/AdminPanel/Table.jsx
+++ app/javascript/src/components/AdminPanel/Table.jsx
@@ -2,36 +2,34 @@ import * as React from "react";
 import { DataGrid } from "@mui/x-data-grid";
 
 const columns = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "firstName", headerName: "First name", width: 130 },
-  { field: "lastName", headerName: "Last name", width: 130 },
+  { field: "id", headerName: "ID", width: 70, headerClassName: "bg-gray-200" },
+  { field: "name", headerName: "Name", width: 130, headerClassName: "bg-gray-200" },
   {
-    field: "age",
-    headerName: "Age",
+    field: "price",
+    headerName: "Price",
     type: "number",
     width: 90,
+    headerClassName: "bg-gray-200"
   },
   {
-    field: "fullName",
-    headerName: "Full name",
-    description: "This column has a value getter and is not sortable.",
-    sortable: false,
+    field: "forRent",
+    headerName: "Is for rent",
+    type: "boolean",
     width: 160,
-    valueGetter: params =>
-      `${params.row.firstName || ""} ${params.row.lastName || ""}`,
+    headerClassName: "bg-gray-200"
   },
 ];
 
 const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  { id: 1, name: "Snow", price: 35, forRent: true },
+  { id: 2, name: "Lannister", price: 42, forRent: false },
+  { id: 3, name: "Lannister", price: 45, forRent: true },
+  { id: 4, name: "Stark", price: 16, forRent: false },
+  { id: 5, name: "Targaryen", price: 77, forRent: true },
+  { id: 6, name: "Melisandre", price: 150, forRent: false },
+  { id: 7, name: "Clifford", price: 44, forRent: true },
+  { id: 8, name: "Frances", price: 36, forRent: false },
+  { id: 9, name: "Roxie", price: 65, forRent: true },
 ];
 
 const Table = () => (
