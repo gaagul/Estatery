bc768f187d5035e1326d752d65fb190c978b8e1a
Gaagul Gigi <70290286+gaagul@users.noreply.github.com>
2023-06-29 14:56:36 +0530

    Added additional info form section and custom avatar component (#23)

--- app/javascript/src/components/Form/AdditionalInfo.jsx
+++ app/javascript/src/components/Form/AdditionalInfo.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React from "react";
+import { Field } from "formik";
+import { Typography } from "antd";
 
-const AdditionalInfo = () => {
-  return (
-    <div>AdditionalInfo</div>
-  )
-}
+const { Title } = Typography;
 
-export default AdditionalInfo
+const AdditionalInfo = () => (
+  <div className="rounded bg-white p-4 pb-8 shadow">
+    <Title className="mb-4" level={2}>
+      Additional information
+    </Title>
+    <div className="grid grid-cols-2 gap-4">
+      <div>
+        <Typography
+          className="mb-2 block font-semibold text-gray-700"
+          htmlFor="price"
+        >
+          Price (Per Month if for Rent)
+        </Typography>
+        <Field
+          className="w-full rounded border border-gray-300 px-4 py-2 focus:border-blue-500 focus:outline-none"
+          id="price"
+          name="price"
+          type="text"
+        />
+      </div>
+      <div>
+        <Typography
+          className="mb-2 block font-semibold text-gray-700"
+          htmlFor="type"
+        >
+          Type (Rent or Sale)
+        </Typography>
+        <Field
+          className="w-full rounded border border-gray-300 px-4 py-2 focus:border-blue-500 focus:outline-none"
+          id="type"
+          name="type"
+          type="text"
+        />
+      </div>
+      <div>
+        <Typography
+          className="mb-2 block font-semibold text-gray-700"
+          htmlFor="description"
+        >
+          Description
+        </Typography>
+        <Field
+          className="w-full rounded border border-gray-300 px-4 py-2 focus:border-blue-500 focus:outline-none"
+          id="description"
+          name="description"
+          type="text"
+        />
+      </div>
+      <div>
+        <Typography
+          className="mb-2 block font-semibold text-gray-700"
+          htmlFor="bedrooms"
+        >
+          Amneties (separate by comma)
+        </Typography>
+        <Field
+          className="w-full rounded border border-gray-300 px-4 py-2 focus:border-blue-500 focus:outline-none"
+          id="amneties"
+          name="amneties"
+          type="text"
+        />
+      </div>
+    </div>
+  </div>
+);
+
+export default AdditionalInfo;
