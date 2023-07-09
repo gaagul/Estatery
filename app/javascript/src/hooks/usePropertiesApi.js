496105f51d997308887e52450452360747c613f9
Gaagul Gigi <70290286+gaagul@users.noreply.github.com>
2023-07-09 19:54:28 +0530

    Updated filtering (#28)

--- app/javascript/src/hooks/usePropertiesApi.js
+++ app/javascript/src/hooks/usePropertiesApi.js
@@ -1,19 +1,19 @@
-import { mergeRight } from "ramda";
 import { useQuery } from "@tanstack/react-query";
-import {fetchAllProperties, fetchPropertyById} from "../apis/properties";
+import { fetchAllProperties, fetchPropertyById } from "../apis/properties";
 
 const STALE_TIME = 100_000;
 const QUERY_KEYS = {
   PROPERTIES_LIST: "propertiesList",
-  PROPERTY_BY_ID: "propertyById"
+  PROPERTY_BY_ID: "propertyById",
 };
 
 const useFetchProperties = () =>
-  useQuery(
-    [QUERY_KEYS.PROPERTIES_LIST],
-    fetchAllProperties, //fetch function in api
-  );
+  useQuery([QUERY_KEYS.PROPERTIES_LIST], fetchAllProperties, {
+    select: data => data?.docs.map(doc => ({ ...doc.data(), id: doc.id })),
+    staleTime: STALE_TIME,
+  });
 
-const useFetchPropertyById = (id) => useQuery([QUERY_KEYS.PROPERTY_BY_ID], ()=>fetchPropertyById(id));
-  
+const useFetchPropertyById = id =>
+  useQuery([QUERY_KEYS.PROPERTY_BY_ID], () => fetchPropertyById(id));
+
 export { useFetchProperties, useFetchPropertyById };
--- app/javascript/src/pages/ListingPage.jsx
+++ app/javascript/src/pages/ListingPage.jsx
@@ -1,55 +1,72 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import { Container } from "@mui/material";
 import { useSearchParams } from "react-router-dom";
-import { pickBy, identity } from "ramda";
+import { pickBy, identity, isEmpty } from "ramda";
 import PropertyCard from "components/PropertyCard";
 import Filterbar from "components/Filterbar";
-import { initializeFilters } from "../utils";
-import { useFetchProperties, useFetchPropertyById } from "../hooks/usePropertiesApi";
-import { Spin } from 'antd';
-
+import { Spin, Typography } from "antd";
+import { initializeFilters, filterProperties } from "../utils";
+import { useFetchProperties } from "../hooks/usePropertiesApi";
 
 const ListingPage = () => {
   const [filters, setFilters] = useState(initializeFilters());
+  const [isFilterBarOpen, setIsFilterBarOpen] = useState(false);
   const [_, setSearchParams] = useSearchParams();
 
-  const { data, status, error } = useFetchProperties();
+  const { data = [], isLoading } = useFetchProperties();
+  const getFilteredProperties = useCallback(
+    () => filterProperties(data, filters),
+    [data, filters]
+  );
+
+  const filteredProperties = getFilteredProperties();
 
   const handleFilterSubmit = values => {
     setFilters(values);
     const nonEmptyFilters = pickBy(identity, values);
 
     setSearchParams(nonEmptyFilters);
+    setIsFilterBarOpen(false);
   };
 
-  if (status === "loading") {
-    <Spin />
-  }
+  const removeQueryParam = key => {
+    const newFilters = { ...filters, [key]: "" };
+    setFilters(newFilters);
+    handleFilterSubmit(newFilters);
+  };
 
-  if (status === "error") {
-    console.log(error);
-  }
-
-  if (status === "success") {
-    console.log(data);
+  if (isLoading) {
     return (
-      <div className="min-h-screen bg-[#f7f7fe]">
-        <Container className="min-h-screen">
-          <div className="flex h-full w-full flex-col px-24 pb-2 pt-16">
-            <Filterbar filters={filters} onSubmit={handleFilterSubmit} />
-            <div className="mt-4 flex flex-grow flex-wrap justify-start gap-6">
-              {data.docs.map(doc => {
-                let property = doc.data();
-                console.log(property);
-                return <PropertyCard key={doc.id} property={property} />
-              }
-              )}
-            </div>
-          </div>
-        </Container>
-      </div>
+      <Spin className="flex h-full w-full flex-col items-center justify-around" />
     );
   }
+
+  return (
+    <div className="min-h-screen bg-[#f7f7fe]">
+      <Container className="min-h-screen">
+        <div className="flex h-full w-full flex-col px-24 pb-2 pt-16">
+          <Filterbar
+            filters={filters}
+            isFilterBarOpen={isFilterBarOpen}
+            removeQueryParam={removeQueryParam}
+            setIsFilterBarOpen={setIsFilterBarOpen}
+            onSubmit={handleFilterSubmit}
+          />
+          <div className="mt-4 flex flex-grow flex-wrap justify-start gap-6">
+            {!isEmpty(filteredProperties) ? (
+              filteredProperties.map(property => (
+                <PropertyCard key={property.id} property={property} />
+              ))
+            ) : (
+              <div className="flex h-full w-full flex-col items-center justify-around">
+                <Typography.Text>No properties found</Typography.Text>
+              </div>
+            )}
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
 export default ListingPage;
