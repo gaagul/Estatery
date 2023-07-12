44c856b83d389cd21404b0e1e5b73941d38df21d
Gaagul Gigi <70290286+gaagul@users.noreply.github.com>
2023-07-12 13:52:58 +0530

    Minor fixes to NavBar ui (#29)

--- app/javascript/src/components/CustomAvatar.jsx
+++ app/javascript/src/components/CustomAvatar.jsx
@@ -25,7 +25,7 @@ const items = [
 ];
 
 const AvatarMenu = () => (
-  <Dropdown menu={{ items }} trigger={["click"]}>
+  <Dropdown menu={{ items }} trigger={["click"]} className="z-50">
     <UserOutlined />
   </Dropdown>
 );
